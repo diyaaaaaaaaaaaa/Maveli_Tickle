ef19b6bb18822e80934e8f6a66d375ce8fcccb7c
Diya P <itsdiyaphere@gmail.com>
2025-08-09 11:47:17 +0530

    update script file

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("tickle-btn").addEventListener("click", function() {
+    let maveli = document.getElementById("maveli");
+
+    // Make Maveli jump
+    maveli.classList.add("jump");
+    // Remove jump after 0.2s so it can trigger again
+    setTimeout(() => {
+        maveli.classList.remove("jump");
+    }, 200);
+});
